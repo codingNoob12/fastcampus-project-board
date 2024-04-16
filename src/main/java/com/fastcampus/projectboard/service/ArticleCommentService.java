addae37534a1d34589386c575bb348a3e1541825
codingNoob12 <ilhyeon0326@gmail.com>
2024-04-16 10:01:50 +0900

    #18 - 댓글 기능 구현 및 전체 테스트 동작 확인
    #25 에서 누락된 댓글 서비스를 마저 구현
    전체 테스트 통과 확인

--- src/main/java/com/fastcampus/projectboard/service/ArticleCommentService.java
+++ src/main/java/com/fastcampus/projectboard/service/ArticleCommentService.java
@@ -1,33 +1,55 @@
 package com.fastcampus.projectboard.service;
 
+import com.fastcampus.projectboard.domain.Article;
+import com.fastcampus.projectboard.domain.ArticleComment;
 import com.fastcampus.projectboard.dto.ArticleCommentDto;
 import com.fastcampus.projectboard.repository.ArticleCommentRepository;
+import com.fastcampus.projectboard.repository.ArticleRepository;
+import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @RequiredArgsConstructor
 @Transactional
 @Service
 public class ArticleCommentService {
 
     private final ArticleCommentRepository articleCommentRepository;
+    private final ArticleRepository articleRepository;
 
     @Transactional(readOnly = true)
     public List<ArticleCommentDto> searchArticleComments(long articleId) {
-        return List.of();
+        return articleCommentRepository.findByArticle_Id(articleId).stream()
+            .map(ArticleCommentDto::from)
+            .collect(Collectors.toList());
     }
 
-    public void saveArticleComment(ArticleCommentDto comment) {
-
+    public void saveArticleComment(ArticleCommentDto dto) {
+        try {
+            Article article = articleRepository.getReferenceById(
+                dto.articleId());
+            articleCommentRepository.save(dto.toEntity(article));
+        } catch (EntityNotFoundException e) {
+            log.warn("댓글 저장 실패. 댓글의 게시글을 찾을 수 없습니다 - dto: {}", dto);
+        }
     }
 
-    public void updateArticleComment(ArticleCommentDto comment) {
-
+    public void updateArticleComment(ArticleCommentDto dto) {
+        try {
+            ArticleComment comment = articleCommentRepository.getReferenceById(
+                dto.id());
+            comment.setContent(dto.content());
+        } catch (EntityNotFoundException e) {
+            log.warn("댓글 업데이트 실패. 댓글을 찾을 수 없습니다 - dto: {}", dto);
+        }
     }
 
     public void deleteArticleComment(Long articleCommentId) {
-
+        articleCommentRepository.deleteById(articleCommentId);
     }
 }
